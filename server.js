8f8ea1d3d4616c84f6746b425ed861844d7aca1b
Nishil Vijay Shah <nishilshah703@gmail.com>
2019-02-01 14:27:15 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require('express');//create web server 
 var morgan = require('morgan');
 var path = require('path');
 
@@ -17,7 +17,16 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
+app.get('/page-one',function(req,res) {
+   res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
+});
+app.get('/page-two',function(req,res) {
+   res.sendFile(path.join(__dirname, 'ui', 'page-two.html'));
+});
+app.get('/page-three',function(req,res) {
+   res.sendFile(path.join(__dirname, 'ui', 'page-three.html'));
+});
+   
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
